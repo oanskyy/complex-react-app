00ab4208fdb758ffcf390887574282afa3e6325d
codessbabe <oana.anghelachi@gmail.com>
2020-11-11 14:28:04 +0100

    Load state from Local Storage

--- complex-react-app/app/components/HeaderLoggedIn.js
+++ complex-react-app/app/components/HeaderLoggedIn.js
@@ -1,6 +1,13 @@
 import React, { useEffect } from "react"
 
 function HeaderLoggedIn(props) {
+  function handleLogout() { 
+    props.setLoggedIn(false)
+    localStorage.removeItem("complexappToken")
+    localStorage.removeItem("complexappUsername")
+    localStorage.removeItem("complexappAvatar")
+  }
+
   return (
     <div className="flex-row my-3 my-md-0">
       <a href="#" className="text-white mr-2 header-search-icon">
@@ -13,13 +20,13 @@ function HeaderLoggedIn(props) {
       <a href="#" className="mr-2">
         <img
           className="small-header-avatar"
-          src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
+          src={localStorage.getItem("complexappAvatar")}
         />
       </a>
       <a className="btn btn-sm btn-success mr-2" href="/create-post">
         Create Post
       </a>
-      <button onClick={() => props.setLoggedIn(false)} className="btn btn-sm btn-secondary">Sign Out</button>
+      <button onClick={handleLogout} className="btn btn-sm btn-secondary">Sign Out</button>
     </div>
   )
 }
