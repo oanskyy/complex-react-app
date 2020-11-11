ab65ccfec4f0d0413371e15629b51287f9a24535
codessbabe <oana.anghelachi@gmail.com>
2020-11-11 14:51:22 +0100

    Create conditional homepage content

--- complex-react-app/app/Main.js
+++ complex-react-app/app/Main.js
@@ -1,20 +1,23 @@
-import React from "react"
+import React, {useState} from "react"
 import ReactDOM from "react-dom"
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 import Header from './components/Header'
 import HomeGuest from './components/HomeGuest'
+import Home from './components/Home'
 import Footer from './components/Footer'
 import About from './components/About'
 import Terms from './components/Terms'
 
 function Main() {
+   const [loggedIn, setLoggedIn] = useState( Boolean(localStorage.getItem("complexappToken")))
+
    return (
    <BrowserRouter>
-      <Header />
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
       <Switch> 
          <Route path="/" exact> 
-            <HomeGuest />
+            {loggedIn ? <Home /> : <HomeGuest />}
          </Route>
          <Route path="/about-us">
             <About />
