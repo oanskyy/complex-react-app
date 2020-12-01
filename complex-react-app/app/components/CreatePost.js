ebc284eff150377ff263af8aa888014c3c214ae1
codessbabe <oana.anghelachi@gmail.com>
2020-12-01 13:10:16 +0100

    Create 'ViewSinglePost' screen

--- complex-react-app/app/components/CreatePost.js
+++ complex-react-app/app/components/CreatePost.js
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from "react"
 import Page from "./Page"
 import Axios from "axios"
+import { withRouter } from "react-router-dom"
 
-function CreatePost() {
+function CreatePost(props) {
   const [title, setTitle] = useState()
   const [body, setBody] = useState()
 
   async function handleSubmit(e) {
     e.preventDefault()
     try {
-      await Axios.post("/create-post", {
+      const response = await Axios.post("/create-post", {
         title,
         body,
         token: localStorage.getItem("complexappToken")
       })
+      // Redirect to new post url
+      props.history.push(`/post/${response.data}`)
       console.log("new post was created")
     } catch (e) {
       console.log("there is a problem")
@@ -58,4 +61,4 @@ function CreatePost() {
   )
 }
 
-export default CreatePost
+export default withRouter(CreatePost)
