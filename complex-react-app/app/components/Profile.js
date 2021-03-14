4d1b47f8e83d9faff21ab0346051d86ff02b9c29
codessbabe <oana.anghelachi@gmail.com>
2021-03-14 22:20:19 +0100

    Load posts by author

--- complex-react-app/app/components/Profile.js
+++ complex-react-app/app/components/Profile.js
@@ -3,6 +3,7 @@ import Page from "./Page"
 import { useParams } from "react-router-dom"
 import Axios from "axios"
 import StateContext from "../StateContext"
+import ProfilePosts from "./ProfilePosts"
 
 function Profile() {
   // destructuring the obj that it gives us and we are only interested in the username prop
@@ -34,7 +35,6 @@ function Profile() {
       <h2>
         <img className="avatar-small" src={profileData.profileAvatar} />
         {profileData.profileUsername}
-        brad
         <button className="btn btn-primary btn-sm ml-2">
           Follow <i className="fas fa-user-plus"></i>
         </button>
@@ -52,32 +52,7 @@ function Profile() {
         </a>
       </div>
 
-      <div className="list-group">
-        <a href="#" className="list-group-item list-group-item-action">
-          <img
-            className="avatar-tiny"
-            src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-          />{" "}
-          <strong>Example Post #1</strong>
-          <span className="text-muted small">on 2/10/2020 </span>
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          <img
-            className="avatar-tiny"
-            src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-          />{" "}
-          <strong>Example Post #2</strong>
-          <span className="text-muted small">on 2/10/2020 </span>
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          <img
-            className="avatar-tiny"
-            src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-          />{" "}
-          <strong>Example Post #3</strong>
-          <span className="text-muted small">on 2/10/2020 </span>
-        </a>
-      </div>
+      <ProfilePosts />
     </Page>
   )
 }
