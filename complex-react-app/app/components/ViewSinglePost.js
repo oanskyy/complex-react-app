4d6a720b68f175fc9b6ef886346fb5441eec6efe
codessbabe <oana.anghelachi@gmail.com>
2021-03-17 17:49:45 +0100

    Delete a post

--- complex-react-app/app/components/ViewSinglePost.js
+++ complex-react-app/app/components/ViewSinglePost.js
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useContext } from "react"
 import Page from "./Page"
-import { useParams, Link } from "react-router-dom"
+import { useParams, Link, withRouter } from "react-router-dom"
 import Axios from "axios"
 import LoadingDotsIcon from "./LoadingDotsIcon"
 import ReactMarkdown from "react-markdown"
 import ReactTooltip from "react-tooltip"
-import NotFound from './NotFound'
+import NotFound from "./NotFound"
+import StateContext from "../StateContext"
+import DispatchContext from "../DispatchContext"
 
-function ViewSinglePost() {
+function ViewSinglePost(props) {
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
   const { id } = useParams()
   const [isLoading, setIsLoading] = useState(true)
   const [post, setPost] = useState()
@@ -32,10 +36,8 @@ function ViewSinglePost() {
     }
   }, [])
 
-  if(!isLoading && !post) {
-    return ( 
-      <NotFound/>
-    )
+  if (!isLoading && !post) {
+    return <NotFound />
   }
 
   if (isLoading)
@@ -50,29 +52,65 @@ function ViewSinglePost() {
     date.getMonth() + 1
   }/${date.getDate()}/${date.getFullYear()}`
 
+  function isOwner() {
+    if (appState.loggedIn) {
+      return appState.user.username == post.author.username
+    }
+    return false
+  }
+
+  async function deleteHandler() {
+    const areYouSure = window.confirm("Do you really want to delete this post")
+    if (areYouSure) {
+      try {
+        const response = await Axios.delete(`/post/${id}`, {
+          data: { token: appState.user.token }
+        })
+        if (response.data == "Success") {
+          // 1. Display a flash message
+          appDispatch({
+            type: "flashMessage",
+            value: "Post was succesfully deleted."
+          })
+          // 2. Redirect back to the current users's profile
+          props.history.push(`/profile/${appState.user.username}`)
+          console.log(
+            `%cThe post was succesfully deleted.`,
+            "font-weight: bold; font-size: 1.5rem; color: green"
+          )
+        }
+      } catch (e) {
+        console.log(`%cThere was a problem`, "font-weight: bold; color: green")
+      }
+    }
+  }
+
   return (
     <Page title={post.title}>
       <div className="d-flex justify-content-between">
         <h2>{post.title}</h2>
-        <span className="pt-2">
-          <Link
-            to={`/post/${post._id}/edit`}
-            data-tip="Edit"
-            data-for="edit"
-            className="text-primary mr-2"
-          >
-            <i className="fas fa-edit"></i>
-          </Link>
-          <ReactTooltip id="edit" className="custom-tooltip" />{" "}
-          <a
-            className="delete-post-button text-danger"
-            data-tip="Delete"
-            data-for="delete"
-          >
-            <i className="fas fa-trash"></i>
-          </a>
-          <ReactTooltip id="delete" className="custom-tooltip" />
-        </span>
+        {isOwner() && (
+          <span className="pt-2">
+            <Link
+              to={`/post/${post._id}/edit`}
+              data-tip="Edit"
+              data-for="edit"
+              className="text-primary mr-2"
+            >
+              <i className="fas fa-edit"></i>
+            </Link>
+            <ReactTooltip id="edit" className="custom-tooltip" />{" "}
+            <a
+              onClick={deleteHandler}
+              className="delete-post-button text-danger"
+              data-tip="Delete"
+              data-for="delete"
+            >
+              <i className="fas fa-trash"></i>
+            </a>
+            <ReactTooltip id="delete" className="custom-tooltip" />
+          </span>
+        )}
       </div>
 
       <p className="text-muted small mb-4">
@@ -104,4 +142,4 @@ function ViewSinglePost() {
   )
 }
 
-export default ViewSinglePost
+export default withRouter(ViewSinglePost)
